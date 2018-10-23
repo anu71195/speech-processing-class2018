d88e03a3ef00583031dfb18a995100ab192d7955
anu <anu71195@gmail.com>
2018-10-23 06:47:21 +0530

    forward procedure done

--- assignment_8/ConsoleApplication16/ConsoleApplication16/ConsoleApplication16.cpp
+++ assignment_8/ConsoleApplication16/ConsoleApplication16/ConsoleApplication16.cpp
@@ -74,7 +74,6 @@ vector< vector <lld> > get_B(string filename, ll M)//getting observation probabi
 {
 	return get_matrix_file_input(filename,M);
 }
-
 vector <lld> get_pi_matrix(string filename)//getting state matrix from input file initial model
 {
 	return get_vector_file_input(filename);
@@ -82,6 +81,68 @@ vector <lld> get_pi_matrix(string filename)//getting state matrix from input fil
 vector<lld> get_observation_sequence(string filename)//getting observation sequence vector from input file initial model
 {
 	return get_vector_file_input(filename);
+}
+void forward_initialization(vector<vector<lld> >&alpha,vector < lld> pi_matrix, vector <vector <lld> > B,vector<lld> O)
+{
+	vector < lld> temp;	
+	for (ll i = 0; i < pi_matrix.size(); i++)		temp.push_back(pi_matrix[i] * B[i][O[0]-1]);
+	alpha.push_back(temp);
+}
+void forward_induction(vector<vector <lld> > &alpha, vector < vector<lld> >& A, vector<vector<lld> >& B, vector <lld>& O,ll &T)
+{
+	vector<lld> alpha_temp;
+	lld temp;
+	for (ll t = 0; t < T - 1; t++)
+	{
+		alpha_temp.clear();
+		for (ll j = 0; j < A.size(); j++)
+		{
+			temp = 0;
+			for (ll i = 0; i < A.size();i++)
+			{
+				temp+=alpha[alpha.size() - 1][i] * A[i][j];
+			}
+			alpha_temp.push_back(temp*B[j][O[t+1]-1]);
+		}
+		alpha.push_back(alpha_temp);
+	}
+}
+void termination(lld &prob,vector<vector<lld> > & alpha)
+{
+	prob = 0;
+	for (ll i = 0; i < alpha[0].size(); i++)prob += alpha[alpha.size()-1][i];
+}
+void forward_procedure(vector < vector <lld> > A, vector < vector < lld> > B, vector <lld> pi_matrix, vector <lld> O,ll T)
+{
+	vector <vector <lld> > alpha;
+	lld P_O_lambda=0;
+	//initialization step
+	forward_initialization(alpha,pi_matrix,B,O);
+	
+	//induction step
+	forward_induction(alpha,A,B,O,T);
+	
+	//terminationa step
+	termination(P_O_lambda,alpha);
+
+	cout << "output of forward procedure is = ";
+	cout << P_O_lambda << endl;
+}
+void backward_initialization(vector<vector<lld> > &beta,ll N)
+{
+	beta.push_back(vector<lld> (N,1));
+}
+void backward_procedure(vector < vector <lld> > A, vector < vector < lld> > B,  vector <lld> O, ll T)
+{
+	vector<vector<lld> > beta;
+	//initialization step
+	backward_initialization(beta,A.size());
+	
+}
+void wrapper(vector < vector <lld> > A, vector < vector < lld> > B, vector <lld> pi_matrix, vector <lld> O, ll T)
+{
+	forward_procedure(A, B, pi_matrix, O, T);
+	backward_procedure(A, B, O, T);
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -95,6 +156,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	pi_matrix=get_pi_matrix(file_pi_matrix);//getting state matrix from input file initial model
 	Observation_sequence1 = get_observation_sequence(file_observation1);
 	Observation_sequence2 = get_observation_sequence(file_observation2);
+	wrapper(A,B,pi_matrix,Observation_sequence1,T);
 	return 0;
 }
 
