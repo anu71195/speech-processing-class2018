be8fc82e8083fab901fa2b2e0f3e1749d01443db
anu <anu71195@gmail.com>
2018-10-23 16:18:53 +0530

    commenting done

--- assignment_8/ConsoleApplication16/ConsoleApplication16/ConsoleApplication16.cpp
+++ assignment_8/ConsoleApplication16/ConsoleApplication16/ConsoleApplication16.cpp
@@ -82,13 +82,13 @@ vector<lld> get_observation_sequence(string filename)//getting observation seque
 {
 	return get_vector_file_input(filename);
 }
-void forward_initialization(vector<vector<lld> >&alpha,vector < lld> pi_matrix, vector <vector <lld> > B,vector<lld> O)
+void forward_initialization(vector<vector<lld> >&alpha,vector < lld> pi_matrix, vector <vector <lld> > B,vector<lld> O)//initialization of forward procedure
 {
 	vector < lld> temp;	
 	for (ll i = 0; i < pi_matrix.size(); i++)		temp.push_back(pi_matrix[i] * B[i][O[0]-1]);
 	alpha.push_back(temp);
 }
-void forward_induction(vector<vector <lld> > &alpha, vector < vector<lld> >& A, vector<vector<lld> >& B, vector <lld>& O,ll &T)
+void forward_induction(vector<vector <lld> > &alpha, vector < vector<lld> >& A, vector<vector<lld> >& B, vector <lld>& O,ll &T)//induction step in forward procedure
 {
 	vector<lld> alpha_temp;
 	lld temp;
@@ -100,19 +100,19 @@ void forward_induction(vector<vector <lld> > &alpha, vector < vector<lld> >& A, 
 			temp = 0;
 			for (ll i = 0; i < A.size();i++)
 			{
-				temp+=alpha[alpha.size() - 1][i] * A[i][j];
+				temp+=alpha[alpha.size() - 1][i] * A[i][j];//summation term
 			}
-			alpha_temp.push_back(temp*B[j][O[t+1]-1]);
+			alpha_temp.push_back(temp*B[j][O[t+1]-1]);//creating vector for every time t is updated
 		}
-		alpha.push_back(alpha_temp);
+		alpha.push_back(alpha_temp);//storing vector for time t in alpha 
 	}
 }
-void termination(lld &prob,vector<vector<lld> > & alpha)
+void termination(lld &prob,vector<vector<lld> > & alpha)//finding probability of observation given model
 {
 	prob = 0;
 	for (ll i = 0; i < alpha[0].size(); i++)prob += alpha[alpha.size()-1][i];
 }
-void forward_procedure(vector < vector <lld> > A, vector < vector < lld> > B, vector <lld> pi_matrix, vector <lld> O,ll T)
+void forward_procedure(vector < vector <lld> > A, vector < vector < lld> > B, vector <lld> pi_matrix, vector <lld> O,ll T)//forward procedure
 {
 	vector <vector <lld> > alpha;
 	lld P_O_lambda=0;
@@ -125,14 +125,15 @@ void forward_procedure(vector < vector <lld> > A, vector < vector < lld> > B, ve
 	//terminationa step
 	termination(P_O_lambda,alpha);
 
-	cout << "output of forward procedure is = ";
-	cout << P_O_lambda << endl;
+	cout << "P(O/lambda) = " << P_O_lambda; nl nl
+	cout << "alpha matrix is"; nl;
+	print_matrix(alpha);
 }
-void backward_initialization(vector<vector<lld> > &beta,ll N)
+void backward_initialization(vector<vector<lld> > &beta,ll N)//initialization of backward procedure
 {
-	beta.push_back(vector<lld> (N,1));
+	beta.push_back(vector<lld> (N,1));//at time t =T all the states have value 1
 }
-void backward_induction(vector<vector<lld> > & beta, vector<vector<lld> > A, vector < vector < lld> > B, vector <lld> O, ll T)
+void backward_induction(vector<vector<lld> > & beta, vector<vector<lld> > A, vector < vector < lld> > B, vector <lld> O, ll T)//induction step in backward procedure
 {
 	vector<lld> b_temp;
 	lld temp = 0;
@@ -144,14 +145,14 @@ void backward_induction(vector<vector<lld> > & beta, vector<vector<lld> > A, vec
 			temp = 0;
 			for (ll j = 0; j < A.size(); j++)
 			{
-				temp += A[i][j] * B[j][O[t] - 1] * beta[0][j];
+				temp += A[i][j] * B[j][O[t] - 1] * beta[0][j];//summation term
 			}
-			b_temp.push_back(temp);
+			b_temp.push_back(temp);//storing values from summation term in vector for every time t for every state
 		}
-		beta.insert(beta.begin(), b_temp);
+		beta.insert(beta.begin(), b_temp);//making a matrix beta by storing values from b_temp vector for each time and each state 1 to T and 1 to N
 	}
 }
-void backward_procedure(vector < vector <lld> > A, vector < vector < lld> > B,  vector <lld> O, ll T)
+void backward_procedure(vector < vector <lld> > A, vector < vector < lld> > B,  vector <lld> O, ll T)//backward procedure
 {
 	vector<vector<lld> > beta;
 	//initialization step
@@ -159,16 +160,18 @@ void backward_procedure(vector < vector <lld> > A, vector < vector < lld> > B,  
 	
 	//induction step
 	backward_induction(beta,A,B,O,T);
+	cout << "alpha matrix is"; nl;
+	print_matrix(beta);
 	
 
 }
-void viterbi_initialization(vector<vector<lld> > &delta, vector< vector < lld> > & psi, vector<vector<lld> > b,vector<lld> pi_matrix,vector<lld> O)
+void viterbi_initialization(vector<vector<lld> > &delta, vector< vector < lld> > & psi, vector<vector<lld> > b,vector<lld> pi_matrix,vector<lld> O)//viterbi algorithm initialization
 {
 	vector<lld> temp;
-	for (int i = 0; i < pi_matrix.size(); i++)		temp.push_back(pi_matrix[i]*b[i][O[0]-1]);
+	for (ll i = 0; i < pi_matrix.size(); i++)		temp.push_back(pi_matrix[i]*b[i][O[0]-1]);
 	delta.push_back(temp), psi.push_back(vector<lld> (pi_matrix.size(),0));
 }
-void viterbi_induction(vector<vector<lld> > &delta, vector < vector < lld> >& psi, vector < vector <lld> > A, vector < vector < lld> > b, vector<lld>O, ll T)
+void viterbi_induction(vector<vector<lld> > &delta, vector < vector < lld> >& psi, vector < vector <lld> > A, vector < vector < lld> > b, vector<lld>O, ll T)//induction step in viterbi algorithm
 {
 	vector<lld> delta_temp,psi_temp;
 	lld temp=INT_MIN,temp2;
@@ -185,37 +188,35 @@ void viterbi_induction(vector<vector<lld> > &delta, vector < vector < lld> >& ps
 				temp2 = delta[delta.size() - 1][i] * A[i][j];
 				if (temp < temp2)
 				{
-					temp = temp2,	index = i;
-					
+					temp = temp2,	index = i;			//max value step and its corresponding index
 				}
-				
 			}
-			delta_temp.push_back(temp*b[j][O[t] - 1]);	
-			psi_temp.push_back(index);
+			delta_temp.push_back(temp*b[j][O[t] - 1]);	//storring max value for each time t and state in delta_temp
+			psi_temp.push_back(index);//and its corresponding index psi_temp
 		}
-		delta.push_back(delta_temp);
-		psi.push_back(psi_temp);
+		delta.push_back(delta_temp);//storing delta_temp vector in delta matrix
+		psi.push_back(psi_temp);//storing psi_temp vector in psi matrix
 	}
 }
-void viterbi_termination(vector< vector< lld> > delta,lld &p_star,lld &q_star)
+void viterbi_termination(vector< vector< lld> > delta,lld &p_star,lld &q_star)//viterbi terimination step
 {
 	p_star = INT_MIN;
-	for (int i = 0; i < delta[delta.size() - 1].size(); i++)
+	for (ll i = 0; i < delta[delta.size() - 1].size(); i++)
 	{
-		if (p_star < delta[delta.size() - 1][i])
+		if (p_star < delta[delta.size() - 1][i])//getting the biggest probability from delta and storing it in p_star and its corresponding index in q_star
 		{
 			p_star = delta[delta.size()-1][i];
 			q_star = i;
 		}
 	}
 }
-vector<lld> viterbi_backtracking(vector<vector<lld> > psi, lld q_star)
+vector<lld> viterbi_backtracking(vector<vector<lld> > psi, lld q_star)//back tracking best state sequence
 {
 	vector< lld > output;
 	for (int t = psi.size() - 2; t >= 0; t--)
 	{
-		output.insert(output.begin(), q_star);
-		q_star = psi[t+1][q_star];
+		output.insert(output.begin(), q_star);//storing q_star or state sequence values in output
+		q_star = psi[t+1][q_star];//from the q_star obtained from the last step getting q_star for previous step
 	}
 	output.insert(output.begin(), q_star);
 	return output;
@@ -237,13 +238,18 @@ void viterbi(vector<vector<lld> > A,vector < vector <lld> > b, vector<lld> pi_ma
 	
 	//backtracking step
 	state_sequence=viterbi_backtracking(psi,q_star);
+	cout << "delta is"; nl
+	print_matrix(delta);
+	cout << "psi is"; nl
+	print_matrix(psi);
+	cout << "state sequence is"; nl
 	print_vector(state_sequence);
 }
 void wrapper(vector < vector <lld> > A, vector < vector < lld> > B, vector <lld> pi_matrix, vector <lld> O, ll T)
 {
-	forward_procedure(A, B, pi_matrix, O, T);
-	backward_procedure(A, B, O, T);
-	viterbi(A,B,pi_matrix,O,T);
+	forward_procedure(A, B, pi_matrix, O, T);//running forward procedure
+	backward_procedure(A, B, O, T);//running backward procedure
+	viterbi(A,B,pi_matrix,O,T);//running viterbi algorithm
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -255,9 +261,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	A=get_A(file_A_matrix,N_states);//getting transition matrix from input file initial model
 	B = get_B(file_B_matrix,N_codebook);//getting observation probability from input file initial model
 	pi_matrix=get_pi_matrix(file_pi_matrix);//getting state matrix from input file initial model
-	Observation_sequence1 = get_observation_sequence(file_observation1);
-	Observation_sequence2 = get_observation_sequence(file_observation2);
-	wrapper(A,B,pi_matrix,Observation_sequence1,T);
+	Observation_sequence1 = get_observation_sequence(file_observation1);//reading observation sequence 1 from file input
+	Observation_sequence2 = get_observation_sequence(file_observation2);//reading observation sequence 2 from file input
+	wrapper(A,B,pi_matrix,Observation_sequence1,T);//runs forward, backward and viterbi algorithm and output the result
 	return 0;
 }
 
